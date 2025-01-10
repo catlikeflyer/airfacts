81c97696dd162a6ef826a473756f8a4f118d0695
Do Hyun Nam <dollis2002@gmail.com>
2025-01-09 22:32:45 -0600

    Updated paths to cypher queries

--- neo4j/loader.py
+++ neo4j/loader.py
@@ -1,5 +1,6 @@
 from neo4j import GraphDatabase
 import pandas as pd
+import os
 
 # Connect to Neo4j
 uri = "bolt://localhost:7687"  # URI to Docker container, local
@@ -9,15 +10,11 @@ password = "airfacts-pw"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
 # Function to read Cypher queries from file
-
-
 def load_query_from_file(filepath):
     with open(filepath, 'r') as file:
         return file.read()
 
 # Function to execute Cypher queries
-
-
 def execute_query(csv_path, cypher_file):
     query = load_query_from_file(cypher_file)
     df = pd.read_csv(csv_path)
@@ -29,18 +26,18 @@ def execute_query(csv_path, cypher_file):
         for _, row in df.iterrows():
             session.run(query, row.to_dict())
 
-
 # Main Script
 if __name__ == "__main__":
     try:
         # File paths
-        airports_csv = "../datasets/airports.csv"
-        airlines_csv = "../datasets/airlines.csv"
-        routes_csv = "../datasets/routes.csv"
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        airports_csv = os.path.join(base_dir, "../datasets/airports.csv")
+        airlines_csv = os.path.join(base_dir, "../datasets/airlines.csv")
+        routes_csv = os.path.join(base_dir, "../datasets/routes.csv")
 
-        airports_cypher = "./load_airport.cypher"
-        airlines_cypher = "./load_airline.cypher"
-        routes_cypher = "./load_route.cypher"
+        airports_cypher = os.path.join(base_dir, "./cypher/load_airport.cypher")
+        airlines_cypher = os.path.join(base_dir, "./cypher/load_airline.cypher")
+        routes_cypher = os.path.join(base_dir, "./cypher/load_route.cypher")
 
         print("Uploading Airports...")
         execute_query(airports_csv, airports_cypher)
